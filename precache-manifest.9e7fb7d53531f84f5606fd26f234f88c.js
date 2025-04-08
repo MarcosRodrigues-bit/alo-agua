c182ebe3332175430764e97af90ccbf454b5a934
unknown <marcosirisrodrigues1997@gmail.com>
2025-04-08 10:37:20 -0300

    Updates

---
+++ precache-manifest.9e7fb7d53531f84f5606fd26f234f88c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67442d25f77533d6ddab0ac6b7795495",
+    "url": "/alo-agua/index.html"
+  },
+  {
+    "revision": "96edf87cb066db1c2aba",
+    "url": "/alo-agua/static/css/main.64caeae6.chunk.css"
+  },
+  {
+    "revision": "bd5be0837c44f5cb7fb5",
+    "url": "/alo-agua/static/js/2.c7099130.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/alo-agua/static/js/2.c7099130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96edf87cb066db1c2aba",
+    "url": "/alo-agua/static/js/main.d5f395fa.chunk.js"
+  },
+  {
+    "revision": "67bb9826510f68b3f5fe",
+    "url": "/alo-agua/static/js/runtime-main.26eaded1.js"
+  },
+  {
+    "revision": "99c380a012bc992841aca823be1bc431",
+    "url": "/alo-agua/static/media/aloagua.99c380a0.png"
+  }
+]);
